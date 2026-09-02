9e92c8f5da379a50efbc1ea8a9eb8e30be6c07e1
bribrax <b.lamassiaude@gmail.com>
2026-09-02 18:18:40 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-/* Configuration du site.
-   Pour activer la synchronisation Google Drive :
-   1. Créez un identifiant OAuth dans la console Google Cloud (voir README).
-   2. Collez-le ci-dessous entre les guillemets, puis publiez sur GitHub. */
-window.GOOGLE_CLIENT_ID = "";
