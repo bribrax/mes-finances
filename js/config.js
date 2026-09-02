cde53c03bc794ae4126f89fd95cafdea7b89f2d3
bribrax <b.lamassiaude@gmail.com>
2026-09-02 18:27:41 +0200

    Set Google Client ID for synchronization

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
    Pour activer la synchronisation Google Drive :
    1. Créez un identifiant OAuth dans la console Google Cloud (voir README).
    2. Collez-le ci-dessous entre les guillemets, puis publiez sur GitHub. */
-window.GOOGLE_CLIENT_ID = "";
+window.GOOGLE_CLIENT_ID = "923812373412-jt4088gp47sfoh1qrjn0mds993qtqan1.apps.googleusercontent.com";
